4e9854c8164d817c007d2f79285ef2a98bfec786
Vadim Zakondyrin <thekondr@gmail.com>
2015-05-01 18:00:15 +0200

    Add time and change git output in zsh prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,8 +1,9 @@
 local return_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})$%{$reset_color%}"
 local current_directory="%{$fg_bold[cyan]%}%c%{$reset_color%}"
-PROMPT='${return_status} ${current_directory}$(git_prompt_info) ${return_status} '
+local current_time="%{$fg_bold[yellow]%}%T%{$reset_color%}"
+PROMPT='${current_time} ${current_directory}$(git_prompt_info) ${return_status} '
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[magenta]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}*%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[yellow]%}*%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
