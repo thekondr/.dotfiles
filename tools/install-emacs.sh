2f0434f808ebf3b4a9ef1cb22254bb6e282b7f26
Vadim Zakondyrin <thekondr@crystalnix.com>
2016-01-07 14:47:10 +0600

    Update spacemacs installation

--- tools/install-emacs.sh
+++ tools/install-emacs.sh
@@ -1,1 +1,2 @@
-echo '(load-file (expand-file-name "~/.dotfiles/spacemacs"))' >> ~/.spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d \
+    && echo '(load-file (expand-file-name "~/.dotfiles/spacemacs"))' >> ~/.spacemacs
