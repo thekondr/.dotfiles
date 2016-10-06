90d3f3c08cc227775a5bfe33d6d5159d4da014b9
Vadim Zakondyrin <thekondr@crystalnix.com>
2016-10-06 08:22:05 +0600

    Update local spacemacs layers according to 0.200

--- emacs/layers/c++-args/keybindings.el
+++ emacs/layers/c++-args/keybindings.el
@@ -1,16 +1,16 @@
-(spacemacs|define-micro-state tk/args
-  :doc (concat "")
+(spacemacs|define-transient-state tk/args
+  :title "C++ args transient state"
   :bindings
-  ("b" tk/backward-arg)
-  ("e" tk/forward-arg)
-  ("x" tk/kill-arg)
-  ("t" tk/transpose-arg)
-  ("s" tk/line-wf-args-switch)
-  (";" tk/comment-dwim-arg)
-  ("u" undo-tree-undo)
-  ("C-r" undo-tree-redo)
-  ("q" nil :exit t)
+  ("b" tk/backward-arg "back")
+  ("e" tk/forward-arg "forward")
+  ("x" tk/kill-arg "kill")
+  ("t" tk/transpose-arg "transpose")
+  ("s" tk/line-wf-args-switch "switch")
+  (";" tk/comment-dwim-arg "comment")
+  ("u" undo-tree-undo "undo")
+  ("C-r" undo-tree-redo "redo")
+  ("q" nil "quit" :exit t)
   )
 
 (spacemacs/set-leader-keys-for-major-mode 'c++-mode
-  "a" 'spacemacs/tk/args-micro-state)
+  "a" 'spacemacs/tk/args-transient-state/body)
