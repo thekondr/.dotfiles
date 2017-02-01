c9b3ef7c5ce8d77c8debf5bc188aa8a5d84feea4
Vadim Zakondyrin <thekondr@crystalnix.com>
2017-02-01 10:32:13 +0600

    Update mergetool

--- emacs/layers/mergetool/funcs.el
+++ emacs/layers/mergetool/funcs.el
@@ -1,3 +1,31 @@
+(defun tool/delete-all-buffers-and-frame()
+  (when ediff-buffer-A (kill-buffer ediff-buffer-A))
+  (when ediff-buffer-B (kill-buffer ediff-buffer-B))
+  (when ediff-buffer-C (kill-buffer ediff-buffer-C))
+  (when ediff-ancestor-buffer (kill-buffer ediff-ancestor-buffer))
+  (when ediff-diff-buffer (kill-buffer ediff-diff-buffer))
+  (when ediff-fine-diff-buffer (kill-buffer ediff-fine-diff-buffer))
+  (when ediff-error-buffer (kill-buffer ediff-error-buffer))
+  (when ediff-control-buffer (kill-buffer ediff-control-buffer))
+  (delete-frame))
+
+(defun mergetool/ediff-quit-merge-hook ()
+  (remove-hook 'ediff-quit-merge-hook 'mergetool/ediff-quit-merge-hook)
+  (let ((file ediff-merge-store-file))
+    (with-current-buffer ediff-buffer-C
+      (write-region (point-min) (point-max) file)
+      (set-buffer-modified-p nil)))
+  (tool/delete-all-buffers-and-frame))
+
+(defun comparetool/ediff-quit-hook ()
+  (remove-hook 'ediff-quit-hook 'comparetool/ediff-quit-hook)
+  (tool/delete-all-buffers-and-frame))
+
+(defun tk/comparetool (file-a file-b)
+  (add-hook 'ediff-quit-hook #'comparetool/ediff-quit-hook)
+  (with-demoted-errors
+      (ediff-files file-a file-b)))
+
 (defun tk/mergetool (file-a file-b file-ancestor file-merge)
   "For use with git mergetool, add these lines to .gitconfig:
 
@@ -15,21 +43,7 @@ or
     (ediff-toggle-show-clashes-only)
     (ediff-next-difference)))
 
-  (add-hook 'ediff-quit-merge-hook (defun mergetool/ediff-quit-merge-hook ()
-    (remove-hook 'ediff-quit-merge-hook 'mergetool/ediff-quit-merge-hook)
-    (let ((file ediff-merge-store-file))
-      (with-current-buffer ediff-buffer-C
-        (write-region (point-min) (point-max) file)
-        (set-buffer-modified-p nil)))
-    (when ediff-buffer-A (kill-buffer ediff-buffer-A))
-    (when ediff-buffer-B (kill-buffer ediff-buffer-B))
-    (when ediff-buffer-C (kill-buffer ediff-buffer-C))
-    (when ediff-ancestor-buffer (kill-buffer ediff-ancestor-buffer))
-    (when ediff-diff-buffer (kill-buffer ediff-diff-buffer))
-    (when ediff-fine-diff-buffer (kill-buffer ediff-fine-diff-buffer))
-    (when ediff-error-buffer (kill-buffer ediff-error-buffer))
-    (when ediff-control-buffer (kill-buffer ediff-control-buffer))
-    (delete-frame)))
+  (add-hook 'ediff-quit-merge-hook #'mergetool/ediff-quit-merge-hook)
 
   (with-demoted-errors
       (ediff-merge-files-with-ancestor file-a file-b file-ancestor nil file-merge)))
