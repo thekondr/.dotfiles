706c87e9297eccb685a8f3184e6264041011ad9d
Vadim Zakondyrin <vadim@termius.com>
2019-02-06 10:40:49 +0600

    Add combine reducers snippet

---
+++ emacs/snippets/utils.el
@@ -0,0 +1,27 @@
+(defun split-imports (string)
+  (mapcar 's-trim (s-split "," string t)))
+
+(defun generate-import (import)
+  (format "import %s from './%s'" import import))
+
+(defun generate-imports (imports)
+  (s-join "\n" (mapcar 'generate-import imports)))
+
+(defun generate-action-state (import)
+  (format
+   "import type { Action as %sAction, State as %sState } from './%s'"
+   (s-upper-camel-case import) (s-upper-camel-case import) import))
+
+(defun generate-actions-states (imports)
+  (s-join "\n" (mapcar 'generate-action-state imports)))
+
+(defun generate-reducers (imports)
+  (s-join "\n" (mapcar '(lambda (i) (format "%s," i)) imports)))
+
+(defun generate-actions (imports)
+  (s-join "\n" (mapcar '(lambda (i) (format "| %sAction" (s-upper-camel-case i))) imports)))
+
+(defun generate-states (imports)
+  (s-join "\n" (mapcar '(lambda (i) (format "%s: %sState," i (s-upper-camel-case i))) imports)))
+
+(provide 'snippets-utils)
