b3c8a636a4b02bd8316a7c132a83a4b46e04d77c
Vadim Zakondyrin <thekondr@gmail.com>
2021-02-17 10:15:24 +0600

    Enable editorconfig

--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -87,6 +87,7 @@
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
